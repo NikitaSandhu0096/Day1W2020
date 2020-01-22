a18fe463fa19347bbdd35f6cb6c3915806df642f
MacStudent <macstudent@128-IMAC20.local>
2020-01-21 20:51:09 -0500

    [NS] Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,9 @@
+package com.lambton;
+
+public class FirstExample {
+    public static void main(String[] args) {
+        System.out.println("Welcome to JAVA Programming");
+        for (int i=1; i<=10; i++)
+            System.out.println("Nikita Sandhu");
+    }
+}
